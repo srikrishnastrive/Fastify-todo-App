72d890b97fdfea6f9b13e3588f2add18fabc54d2
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-05-30 19:34:11 +0530

    added the all api's

--- src/routes/api/v1/todos/index.js
+++ src/routes/api/v1/todos/index.js
@@ -1,9 +1,11 @@
-const { getAllTodos, createTodo,getTodoById } = require("../../../../controllers/todoController");
+const { getAllTodos, createTodo,getTodoById,deleteTodoById,deleteAllTodo} = require("../../../../controllers/todoController");
 
 async function todoRouter(fastify,options){
     fastify.get('/',getAllTodos);
     fastify.post('/create',createTodo);
     fastify.get('/:id',getTodoById);
+    fastify.delete('/:id',deleteTodoById);
+    fastify.delete('/deleteAll',deleteAllTodo);
     
 }
 
